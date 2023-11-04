2b5ed8daa428538a347008bb6a431468d7355f31
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2023-11-04 13:52:56 -0400

    Completed ternary operator rewrite stretch goal

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -123,9 +123,8 @@ switch (luckyNumber) {
 }
 console.log(luckyResult);
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
-
-
-
+let resultTwo = adventurous === true ? 'Adventures are great!' : 'How about we stay home?';
+console.log(resultTwo)
 
 
 
